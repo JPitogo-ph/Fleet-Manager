75d3d3b9e459c0ce8a32b9c6a6bf23c67bcc7361
JPitogo-ph <jpitogodev@gmail.com>
2026-05-04 23:39:00 +0800

    Commit types for Trip module, implement schema for GET query as there are multiple possible params and each needs validation.

---
+++ backend/src/modules/trip/trip.types.ts
@@ -0,0 +1,21 @@
+import z from "zod";
+import { TripStatus } from "../../../generated/prisma/enums.js";
+
+const CreateTripSchema = z
+  .object({
+    vehicleId: z.uuid(),
+    driverId: z.uuid,
+    originAddress: z.string().min(1),
+    destinationAddress: z.string().min(1),
+    scheduledAt: z.iso.datetime(),
+    notes: z.string().optional(),
+  })
+  .strict();
+
+const GetTripsSchema = z.object({
+  status: z.enum(TripStatus).optional(),
+  vehicleId: z.uuid().optional(),
+  driverId: z.uuid().optional(),
+});
+
+export type CreateTripInput = z.infer<typeof CreateTripSchema>;
